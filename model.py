c58db952a8616a788ecb21e933380661808577e6
einanao <nanaoei@protonmail.com>
2022-07-30 17:32:06 -0400

    make nn wider

--- model.py
+++ model.py
@@ -5,11 +5,11 @@ import torch.nn.functional as F
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.fc1 = nn.Linear(5, 3)
-        self.fc2 = nn.Linear(3, 5)
-        self.fc3 = nn.Linear(5, 2)
+        self.fc1 = nn.Linear(5, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 2)
         self.act = F.leaky_relu
-        self.drop = nn.Dropout(0.25)
+        self.drop = nn.Dropout(0.025) # DEBUG
 
     def forward(self, x):
         x = self.act(self.drop(self.fc1(x)))
